17d38f050a54e250f9d7c9cbcbb119181cf6b50d
Globson <samuelpedro1999@gmail.com>
2018-09-04 20:18:29 -0300

    Corrigido os erros de chamada de funções e definido conteudo de funções.

--- Sources/Voo.c
+++ Sources/Voo.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void InicializaVoo(Voo* Modelo) {
+void InicializaVoo(Voo* Modelo){
   Modelo->VID = 0;
   Modelo->Hora_Decola = 0;
   Modelo->Minuto_Decola = 0;
@@ -11,30 +11,30 @@ void InicializaVoo(Voo* Modelo) {
     Modelo->Minuto_Previsto= 0;
   Modelo->Pista = 0;
   strcpy(Modelo->Aeroporto_Chegada,"");
-  strcpy(Modelo->Aeroporto_Partida,"");
-
-}
-void SetVID(Voo* Modelo,int Hora_decolagem,int Hora_de_pouso,int Hora_previsto,int Numero_Pista){
-int i;
-  srand(time(NULL));
-  Modelo->VID(%d,rand()%1000);
-  printf("Horario do voo, Horas,Minutos\n");
-  scanf("%d, %d\n",Modelo->Hora_Decola, Modelo->Minuto_Decola );
-  printf("Horario previsto de chegada, Horas,Minutos\n");
-  scanf("%d, %d\n",Modelo->Hora_Previsto, Modelo->Minuto_Previsto );
-  printf("Aeroporto de partida?\n");
-  scanf("30%s\n", Aeroporto_Partida );
-  printf("Aeroporto de chegada?\n");
-  scanf("30%s\n", Aeroporto_Chegada );
-  printf("Pista de decolagem?\n");
-  scanf("%d\n",Modelo->Pista );
-
-}  //Variaveis declaradas para atribuições dentro da struct.
-void GetVID(Voo Modelo){printf("%d",Modelo.VID);}
-void GetHora_Decola(Voo Modelo){printf("%d",Modelo.Hora_Decola);}
-void GetHora_Decola(Voo Modelo){printf("%d",Modelo.Minuto_Decola);}
-void GetHora_Previsto(Voo Modelo){printf("%d",Modelo.Hora_Previsto);}
-void GetHora_Decola(Voo Modelo){printf("%d",Modelo.Minuto_Previsto);}
-void GetPista(Voo Modelo){printf("%d",Modelo.Pista);}
-void GetHora_Partida(Voo Modelo){printf("%s",Modelo.Aeroporto_Partida);}
-void GetAeroporto_Chegada(Voo Modelo){printf("%s",Modelo.Aeroporto_Chegada);}
+  strcpy(Modelo->Aeroporto_Partida,"");}
+void SetVID(Voo* Modelo){Modelo->VID=(rand()%1000);}
+void SetHora_Minuto_Decola(Voo* Modelo){
+  printf("Horas da partida:");
+  scanf("%d",&Modelo->Hora_Decola);
+  printf("\nMinutos da partida:");
+  scanf("%d",&Modelo->Minuto_Decola);}
+void SetHora_Minuto_Previsto(Voo* Modelo){
+  printf("Horas da chegada prevista:");
+  scanf("%d",&Modelo->Hora_Previsto);
+  printf("\nMinutos da chegada prevista:");
+  scanf("%d",&Modelo->Minuto_Previsto);}
+void SetPista(Voo* Modelo){
+  printf("\nPista de decolagem?:");
+  scanf("%d",&Modelo->Pista );}
+void SetAeroporto_Partida(Voo* Modelo){
+  printf("\nAeroporto de partida:");
+  fgets(Modelo->Aeroporto_Partida,30,stdin);}
+void SetAeroporto_Chegada(Voo* Modelo){
+  printf("\nAeroporto de chegada:");
+  fgets(Modelo->Aeroporto_Chegada,30,stdin);}
+void GetVID(Voo Modelo){printf("\n%d",Modelo.VID);}
+void GetHora_Minuto_Decola(Voo Modelo){printf("\n%d:%d",Modelo.Hora_Decola,Modelo.Minuto_Decola);}
+void GetHora_Minuto_Previsto(Voo Modelo){printf("\n%d:%d",Modelo.Hora_Previsto,Modelo.Minuto_Previsto);}
+void GetPista(Voo Modelo){printf("\nPista:%d",Modelo.Pista);}
+void GetAeroporto_Partida(Voo Modelo){printf("\n%s",Modelo.Aeroporto_Partida);}
+void GetAeroporto_Chegada(Voo Modelo){printf("\n%s",Modelo.Aeroporto_Chegada);}
