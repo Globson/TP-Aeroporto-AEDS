d008eb5c32a386df261d2a7713c8a0a6289c2a91
Numb4r <yuri.d.d.faria@gmail.com>
2018-09-06 23:31:56 +0000

    Adjusted functions parameters

--- Libs/Voo.h
+++ Libs/Voo.h
@@ -8,16 +8,22 @@ typedef struct DadoVoo{
   char Aeroporto_Partida[30];
   char Aeroporto_Chegada[30];
 } Voo;
+//Inicializacao
 void InicializaVoo(Voo* Modelo);
+//------------------------------------------------------------------------------------------------------
+//SETS
 void SetVID(Voo* Modelo);  //Variaveis declaradas para atribuições dentro da struct.
-void SetHora_Minuto_Decola(Voo* Modelo);
-void SetHora_Minuto_Previsto(Voo* Modelo);
-void SetPista(Voo* Modelo);
-void SetAeroporto_Partida(Voo* Modelo);
-void SetAeroporto_Chegada(Voo* Modelo);
-void GetVID(Voo Modelo);
-void GetHora_Minuto_Decola(Voo Modelo);
-void GetHora_Minuto_Previsto(Voo Modelo);
-void GetPista(Voo Modelo);
-void GetAeroporto_Partida(Voo Modelo);
-void GetAeroporto_Chegada(Voo Modelo);
+void SetHora_Minuto_Decola(Voo* Modelo,int hora,int minuto);
+void SetHora_Minuto_Previsto(Voo* Modelo,int hora,int minuto);
+void SetPista(Voo* Modelo,int pista);
+void SetAeroporto_Partida(Voo* Modelo,char aeroporto[]);
+void SetAeroporto_Chegada(Voo* Modelo,char aeroporto[]);
+//------------------------------------------------------------------------------------------------------
+//GETS
+int GetVID(Voo Modelo);
+void GetHora_Minuto_Decola(Voo Modelo,int hora,int minuto);
+void GetHora_Minuto_Previsto(Voo Modelo,int hora,int minuto);
+int GetPista(Voo Modelo);
+char* GetAeroporto_Partida(Voo Modelo);
+char* GetAeroporto_Chegada(Voo Modelo);
+//------------------------------------------------------------------------------------------------------
