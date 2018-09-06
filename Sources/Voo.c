eed4540a615d7d16af060b4b84e9c4c70ae68bf8
Numb4r <yuri.d.d.faria@gmail.com>
2018-09-06 20:47:54 +0000

    Fix Sets  don't worked as are projected to

--- Sources/Voo.c
+++ Sources/Voo.c
@@ -1,4 +1,3 @@
-
 #include "../Libs/Voo.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,40 +11,40 @@ void InicializaVoo(Voo* Modelo){
   Modelo->Hora_Decola = 0;
   Modelo->Minuto_Decola = 0;
   Modelo->Hora_Previsto = 0;
-    Modelo->Minuto_Previsto= 0;
+  Modelo->Minuto_Previsto= 0;
   Modelo->Pista = 0;
   strcpy(Modelo->Aeroporto_Chegada,"");
   strcpy(Modelo->Aeroporto_Partida,"");}
 //------------------------------------------------------------------------------------------------------
 
 // SETS
+//TODO:Colocar esse trecho no main para a leitura dos : no horario
+//char p1[1];
+//printf("Entre com a hora e os minutos da partida separados por dois pontos:");
+//scanf("%d%c%d",&Modelo->Hora_Decola,p1,&Modelo->Minuto_Decola);}
 void SetVID(Voo* Modelo){
   srand(time(NULL));
   Modelo->VID=(rand()%1000);}
-void SetHora_Minuto_Decola(Voo* Modelo){
-  char p1[1];
-  printf("Entre com a hora e os minutos da partida separados por dois pontos:");
-  scanf("%d%c%d",&Modelo->Hora_Decola,p1,&Modelo->Minuto_Decola);}
-void SetHora_Minuto_Previsto(Voo* Modelo){
-  char p2[1];
-  printf("Entre com a hora e minutos de chegada prevista separados por dois pontos:");
-  scanf("%d%c%d",&Modelo->Hora_Previsto,p2,&Modelo->Minuto_Previsto);}
+void SetHora_Minuto_Decola(Voo* Modelo, int hora,int minuto){
+  Modelo->Hora_Decola = hora;
+  Modelo->Minuto_Decola = minuto;
+void SetHora_Minuto_Previsto(Voo* Modelo,int hora,int minuto){
+  Modelo->Hora_Previsto = hora;
+  Modelo->Minuto_Previsto = minuto;}
 void SetPista(Voo* Modelo){
-  printf("\nPista de decolagem?:");
-  scanf("%d",&Modelo->Pista );}
+  Modelo->Pista;}
 void SetAeroporto_Partida(Voo* Modelo){
-  printf("\nAeroporto de partida:");
-  fgets(Modelo->Aeroporto_Partida,30,stdin);}
+  Modelo->Aeroporto_Partida;}
 void SetAeroporto_Chegada(Voo* Modelo){
-  printf("\nAeroporto de chegada:");
-  fgets(Modelo->Aeroporto_Chegada,30,stdin);}
+  Modelo->Aeroporto_Chegada;}
 //------------------------------------------------------------------------------------------------------
 
 // GETS
-void GetVID(Voo Modelo){printf("\n%d",Modelo.VID);}
-void GetHora_Minuto_Decola(Voo Modelo){printf("\n%d:%d",Modelo.Hora_Decola,Modelo.Minuto_Decola);}
-void GetHora_Minuto_Previsto(Voo Modelo){printf("\n%d:%d",Modelo.Hora_Previsto,Modelo.Minuto_Previsto);}
-void GetPista(Voo Modelo){printf("\nPista:%d",Modelo.Pista);}
-void GetAeroporto_Partida(Voo Modelo){printf("\n%s",Modelo.Aeroporto_Partida);}
-void GetAeroporto_Chegada(Voo Modelo){printf("\n%s",Modelo.Aeroporto_Chegada);}
+//TODO:Retornar ao invez de printar os valores repassados
+void GetVID(Voo Modelo){printf("%d",Modelo.VID);}
+void GetHora_Minuto_Decola(Voo Modelo){printf("%d:%d",Modelo.Hora_Decola,Modelo.Minuto_Decola);}
+void GetHora_Minuto_Previsto(Voo Modelo){printf("%d:%d",Modelo.Hora_Previsto,Modelo.Minuto_Previsto);}
+void GetPista(Voo Modelo){printf("%d",Modelo.Pista);}
+void GetAeroporto_Partida(Voo Modelo){printf("%s",Modelo.Aeroporto_Partida);}
+void GetAeroporto_Chegada(Voo Modelo){printf("%s",Modelo.Aeroporto_Chegada);}
 //------------------------------------------------------------------------------------------------------
