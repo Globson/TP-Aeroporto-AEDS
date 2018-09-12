e48e3898703f84ee327b762a06733800acdbc05a
Globson <samuelpedro1999@gmail.com>
2018-09-12 19:01:03 -0300

    Formatação dos nomes das funções

--- Libs/ItemMatriz.h
+++ Libs/ItemMatriz.h
@@ -8,11 +8,11 @@ typedef struct{
 void InicializaMatriz(Item_Matriz* Modelo);
 //------------------------------------------------------------------------------------------------------
 //GETS
-void GetListadeVoos(Item_Matriz* Modelo);
-void GetNumerodeVoos(Item_Matriz* Modelo);
-void GetUltimaAtualizacao(Item_Matriz* Modelo);
+void GetLista_de_Voos(Item_Matriz* Modelo);
+void GetNumero_de_Voos(Item_Matriz* Modelo);
+void GetUltima_Atualizacao(Item_Matriz* Modelo);
 //------------------------------------------------------------------------------------------------------
 //SETS
-void SetListadeVoos(Item_Matriz* Modelo, ListaVoos lista);
-void SetNumerodeVoos(Item_Matriz* Modelo,int numero_voos);
-void SetUltimaAtualizacaoO(Item_Matriz* Modelo, int ultima_atualizacao);
+void SetLista_de_Voos(Item_Matriz* Modelo, ListaVoos lista);
+void SetNumero_de_Voos(Item_Matriz* Modelo,int numero_voos);
+void SetUltima_AtualizacaoO(Item_Matriz* Modelo, int ultima_atualizacao);
