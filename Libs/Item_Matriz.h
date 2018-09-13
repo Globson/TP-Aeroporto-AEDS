79ab97839c5922516dfc85b29d6b95036906d819
Globson <samuelpedro1999@gmail.com>
2018-09-13 00:33:22 -0300

    Linkei de acordo a lista encadeada nesse TAD.<3

--- Libs/Item_Matriz.h
+++ Libs/Item_Matriz.h
@@ -1,6 +1,6 @@
-#include "ListaVoos.h"
-typedef struct{
-    ListaVoos lista;
+#include "Lista_de_Voos.h"
+typedef struct ItemMatriz{
+    TLista* pLista;
     int Numero_de_Voos;
     int Ultima_Atualizacao;
 }Item_Matriz;
@@ -13,6 +13,6 @@ void GetNumero_de_Voos(Item_Matriz* Modelo);
 void GetUltima_Atualizacao(Item_Matriz* Modelo);
 //------------------------------------------------------------------------------------------------------
 //SETS
-void SetLista_de_Voos(Item_Matriz* Modelo, ListaVoos lista);
+void SetLista_de_Voos(Item_Matriz* Modelo, TLista* pLista);
 void SetNumero_de_Voos(Item_Matriz* Modelo,int numero_voos);
 void SetUltima_AtualizacaoO(Item_Matriz* Modelo, int ultima_atualizacao);
