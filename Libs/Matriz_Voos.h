5f6874bb692b1e0246da1d3ad8b074795e244639
Numb4r <yuri.d.d.faria@gmail.com>
2018-09-16 01:53:32 +0000

    Arrumado os parametros

--- Libs/Matriz_Voos.h
+++ Libs/Matriz_Voos.h
@@ -7,33 +7,37 @@ typedef struct {
   int Total_De_Voos;
   char Data[20];
   char Hora_Ultima_Atualizacao[20];
-}TMatriz;
+}tMatriz;
 
 
 
 //------------------------------------------------------------------------------------------------------
 
 // SETS
-void SetData(TMatriz* pMatriz);
-void SetTotal_De_Voos(TMatriz* pMatriz);
-void SetHora_Ultima_Atualizacao(TMatriz* pMatriz);
+void SetData(tMatriz* pMatriz);
+void SetTotal_De_Voos(tMatriz* pMatriz);
+void SetHora_Ultima_Atualizacao(tMatriz* pMatriz);
 
 
 // GETS
+char* GetData(tMatriz* pMatriz);
+char* GetHora_Ultima_Atualizacao(tMatriz* pMatriz);
 
-void Inicia_Matriz(TMatriz* pMatriz);
-int Insere_Voo_Matriz(TMatriz* pMatriz,TVoo* Voo);
-TVoo* Remove_Voo_Matriz(TMatriz* pMatriz,int VID);
-TVoo* Procurar_Voo_Matriz(TMatriz* pMatriz,int VID);
-void Imprimir_Voos_Decolagem_Pouso(TMatriz* pMatriz,char Hora_Decola,char Hora_Pouso);
-void Imprimir_Voos_Decolagem(TMatriz* pMatriz);
-void Imprimir_Voos_Pouso(TMatriz* pMatriz);
-void Imprimir_Matriz(TMatriz* pMatriz);
-void Encontrar_Faixa_Voos_Maior(TMatriz* pMatriz);//Faixa que tem a maior quantidade de voos
-void Encontrar_Faixa_Voos_Menor(TMatriz* pMatriz);//Faixa que tem a menor quantidade de voos
-void Encontrar_Lista_Voos_Mais_Recente(TMatriz* pMatriz);
-void Encontrar_Lista_Voos_Menos_Recente(TMatriz* pMatriz);
-int Matriz_Esparca(TMatriz* pMatriz);
+//------------------------------------------------------------------------------------------------------
+//Funcoes
+void Inicia_Matriz(tMatriz* pMatriz);
+int Insere_Voo_Matriz(tMatriz* pMatriz,TVoo* Voo);
+TVoo* Remove_Voo_Matriz(tMatriz* pMatriz,int VID);
+TVoo* Procurar_Voo_Matriz(tMatriz* pMatriz,int VID);
+void Imprimir_Voos_Decolagem_Pouso(tMatriz* pMatriz,char* Hora_Decola,char* Hora_Pouso);
+void Imprimir_Voos_Decolagem(tMatriz* pMatriz,char* Hora_Decola);
+void Imprimir_Voos_Pouso(tMatriz* pMatriz,char* Hora_Previsto);
+void Imprimir_Matriz(tMatriz* pMatriz);
+void Encontrar_Faixa_Voos_Maior(tMatriz* pMatriz);//Faixa que tem a maior quantidade de voos
+void Encontrar_Faixa_Voos_Menor(tMatriz* pMatriz);//Faixa que tem a menor quantidade de voos
+void Encontrar_Lista_Voos_Mais_Recente(tMatriz* pMatriz);
+void Encontrar_Lista_Voos_Menos_Recente(tMatriz* pMatriz);
+int Matriz_Esparca(tMatriz* pMatriz);
 /*Verificar se o numero de posicoes da matriz sem voos
 cadastrados e,no minimo,o dobro daquelas com 1 voo
 cadastrado*/
