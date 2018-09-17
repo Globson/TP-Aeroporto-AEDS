e2488d0ff7ac77d9451f2bf1819e2cbbb6cf08d8
Globson <samuelpedro1999@gmail.com>
2018-09-17 17:52:35 -0300

    Criado teste

---
+++ Testes dos TADS Separados/Teste_Matriz.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../Libs/Matriz_Voos.h"
+#include "../Libs/Item_Matriz.h"
+#include "../Libs/Lista_de_Voos.h"
+#include "../Libs/Dados_Voo.h"
+tMatriz Matriz;
+SetData(&Matriz);
