8f10a3cae3ca7112b0d834b0ca7a0ad960d3ea33
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2018-09-18 11:26:26 -0300

    Menu criado

--- Libs/Menu.h
+++ Libs/Menu.h
@@ -1,3 +1,7 @@
+#ifndef MENU_H
+#define MENU_H
 void Cabecalho();
 void MenuArquivoInteraivo();
+void MenuInteraivo();
 void Rodape();
+#endif
--- Sources/Menu.c
+++ Sources/Menu.c
@@ -1,5 +1,28 @@
+#include <stdio.h>
+#include "../Libs/Menu.h"
 void Cabecalho(){
   printf("\n\n###########################################################################\n ");
-  printf("|\t\tTAD VOO\t\t|\n");
+  printf("\t\t\tBem vindo ao TAD VOO\n");
   printf("###########################################################################\n\n ");
+
+}
+void Rodape(/* arguments */) {
+  printf("\n\n###########################################################################\n\n");
+}
+void MenuArquivoInteraivo(/* arguments */) {
+  Cabecalho();
+  printf("\t1.Entrada interativa\n");
+  printf("\t2.Entrada por arquivo\n");
+  Rodape();
+}
+void MenInteraivo(/* arguments */) {
+  Cabecalho();
+  printf("\t1.Inserir Voo\n");
+  printf("\t2.Remove Voo\n");
+  printf("\t3.Procura Voo\n");
+  printf("\t4.Imprimir Voo\n");
+  printf("\t5.Encontrar faixa\n" );
+  printf("\t6.Verificar matriz esparca\n" );
+  printf("\t7.Entrada por arquivo\n");
+  Rodape();
 }
